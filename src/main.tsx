574165a61e40a6c2cccd440cbd00658f5cf9fd71
Brian McClune <brian.mcclune@proton.me>
2024-12-03 16:13:35 -0500

    Add FluentUI, wrap App with FluentProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import { FluentProvider } from "@fluentui/react-components";
+
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <FluentProvider>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>,
+  </FluentProvider>
 );
